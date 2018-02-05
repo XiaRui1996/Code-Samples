1e66d5264f48789dbaf516a7d4829c318266a9db
XiaRui1996 <xr2739401611@gmail.com>
2018-02-05 17:32:36 +0800

    Add files via upload

---
+++ digit/results/First Attempt.py
@@ -0,0 +1,231 @@
+
+# coding: utf-8
+
+# Importing Necessary Modules
+
+# In[61]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+
+import keras
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from keras.optimizers import RMSprop, Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+
+# Loading the dataset
+
+# In[48]:
+
+
+train = pd.read_csv("train.csv")
+print('train shape:',train.shape)
+train.head()
+
+
+# In[49]:
+
+
+test = pd.read_csv("test.csv")
+print('test shape:',test.shape)
+test.head()
+
+
+# In[50]:
+
+
+
+
+# Checking for missing values
+
+
+
+# Dataset image preview
+
+# In[53]:
+
+
+y_train = train["label"]
+x_train = train.drop(labels = ["label"],axis = 1) 
+del train 
+x_test = test
+del test
+
+
+# In[54]:
+
+
+# In[55]:
+
+
+
+
+# Data Normalization
+
+# In[56]:
+
+
+x_train = x_train/255.0
+x_test = x_test/255.0
+
+
+# Reshaping Data
+
+# In[58]:
+
+
+X_train = x_train.values.reshape(x_train.shape[0], 28, 28,1)
+X_test = x_test.values.reshape(x_test.shape[0], 28, 28,1)
+
+
+# In[59]:
+
+
+batch_size = 64
+num_classes = 10
+epochs = 10
+input_shape = (28, 28, 1)
+
+
+# In[62]:
+
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+#Converts a class vector (integers) to binary class matrix.E.g. for use with categorical_crossentropy.
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, y_train, test_size = 0.1, random_state=42)
+
+
+# __Linear Model__
+
+# In[ ]:
+
+
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5, 5), activation='relu', padding = 'same',
+                 kernel_initializer='glorot_uniform',
+                 input_shape=input_shape))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2)))
+model.add(Dropout(0.25))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3, 3), activation='relu',padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.RMSprop(),
+              metrics=['accuracy'])
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.0001)
+
+datagen = ImageDataGenerator(
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1)  # randomly flip images
+
+model.summary()
+
+
+
+datagen.fit(X_train)
+
+
+
+h = model.fit_generator(datagen.flow(X_train,Y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (X_val,Y_val),
+                              verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
+
+
+
+
+
+final_loss, final_acc = model.evaluate(X_val, Y_val, verbose=0)
+print("Final loss: {0:.6f}, final accuracy: {1:.6f}".format(final_loss, final_acc))
+
+
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Predict the values from the validation dataset
+Y_pred = model.predict(X_val)
+# Convert predictions classes to one hot vectors 
+Y_pred_classes = np.argmax(Y_pred, axis = 1) 
+# Convert validation observations to one hot vectors
+Y_true = np.argmax(Y_val, axis = 1) 
+# compute the confusion matrix
+confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+# plot the confusion matrix
+plot_confusion_matrix(confusion_mtx, classes = range(10))
+plt.show()
+
+
+
+fig, ax = plt.subplots(2,1)
+ax[0].plot(h.history['loss'], color='b', label="Training loss")
+ax[0].plot(h.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(h.history['acc'], color='b', label="Training accuracy")
+ax[1].plot(h.history['val_acc'], color='r',label="Validation accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
+plt.show()
+
+
